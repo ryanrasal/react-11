4546bfa3533cd114a4d50a27959f58530a177803
ryanrasal <112951711+ryanrasal@users.noreply.github.com>
2022-10-12 20:31:48 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function SignupForm() {
-  const [value, setValue] = React.useState();
+ function SignupForm() {
+ const [name, email] = useState("");
+ console.log(`name : ${name.state}, email : ${email.state}`)
   return (
-    <div>
-      <input value={value} onChange={(e) => setValue(e.target.value)} />
-    </div>
+    <form>
+      <label htmlFor="name">
+        Name :
+        <input type="text" name="name" id="name" placeholder="Mary" />
+      </label>
+      <br />
+      <label htmlFor="email">
+        Email :
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="mary.poppins@disney.fr"
+        />
+      </label>
+      <br />
+      <br />
+      <button type="submit" onClick={() => SignupForm()}>
+        Submit
+      </button>
+    </form>
   );
-
-  const display = (e) => {
-    e.preventDefault();
-    console.log('name & email here');
-  };
 }
-return (
-  <form>
-    <label htmlFor="name">
-      Name :
-      <input type="text" name="name" id="name" placeholder="Mary" />
-    </label>
-    <br />
-    <label htmlFor="email">
-      Email :
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="mary.poppins@disney.fr"
-      />
-    </label>
-    <br />
-    <br />
-    <button type="submit" onClick={() => display()}>
-      Submit
-    </button>
-  </form>
-);
+
 
 export default SignupForm;
